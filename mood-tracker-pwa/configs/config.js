d37b71c2990f66d7ffd6ac8e20dff13c0898f950
David Lora <despinal0425@gmail.com>
2022-11-28 08:56:48 -0500

    created and style homepage

---
+++ mood-tracker-pwa/configs/config.js
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+/**
+ * @namespace
+ * @property {string}  config.API_KEY         - The API key.
+ * @property {string}  config.URL_ADDRESS     - The URL to the API.
+ */
+const config = { API_KEY: '', URL_ADDRESS: '' };
