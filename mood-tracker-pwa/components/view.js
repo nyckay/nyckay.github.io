f3e5bd71d462433e5bd8c741b48956c7eaaa2b19
David Lora <despinal0425@gmail.com>
2023-01-29 15:09:45 -0500

    attribute values can be now be retrieved from each items and passed in to the modal card

--- mood-tracker-pwa/components/view.js
+++ mood-tracker-pwa/components/view.js
@@ -32,12 +32,14 @@ template.innerHTML = `
 
   <!-- Modal content -->
   <div class="modal-content">
-    <span class="close">&times;</span>
-    
-    <p>Some text in the Modal..</p>
+    <span class="close">&times;</span> 
+    <p class="time-stamp"></p> 
+    <div>
+      <div class="moodTitle"></div>
+      <div class="entryText"></div>
+     </div>
+    </div>
   </div>
-
-</div>
 `;
 
 /** Class representing a view button. */
@@ -60,14 +62,49 @@ class View extends HTMLElement {
 
     // Get the <span> element that closes the modal
     this.span = this.shadowRoot.querySelector(".close");
+    
+    // Get <div> element with the class moodTitle & entryText
+    this.moodTitle = this.shadowRoot.querySelector(".moodTitle")
+    this.viewEntry = this.shadowRoot.querySelector(".entryText");
 
+    // Get <div> element with the class time stamp
+    this.timeStamp = this.shadowRoot.querySelector(".time-stamp");
   }
+
+  // Observe timestamp, entrytitle, entrytext, & entrymood attributes for changes
+  static get observedAttributes() {
+    return ["timestamp", "entrytitle", "entrytext", "entrymood"];
+   }
   
   /**
      * track element's attribute update: changed, added, or removed
     */
   attributeChangedCallback(property, oldValue, newValue) { 
     // If nothing changes, stop execution
+    // eslint-disable-next-line no-useless-return
+    if (oldValue === newValue) return;
+
+
+    // Conditional statement will look at the name of the atttribute and it's value in view-modal element. If matched, add attribute value to text content of element created and appended to the modal content
+    if (property === "entrymood") {
+      const headerTwo = document.createElement("h2");
+      headerTwo.textContent = `${newValue}`;
+      this.moodTitle.appendChild(headerTwo);
+    } else if (property === "timestamp") {
+      // const pOne = document.createElement("p");
+      // pOne.textContent = `${newValue}`;
+      this.timeStamp.textContent = `${newValue}`;
+    } else if (property === "entrytitle") {
+      const headerThree = document.createElement("h3");
+      headerThree.textContent = `${newValue}`;
+      this.viewEntry.appendChild(headerThree);
+    } else if (property === "entrytext") {
+      const pTwo = document.createElement("p");
+      pTwo.textContent = `${newValue}`;
+      this.viewEntry.appendChild(pTwo);
+    }
+   
+    
   }
 
   // Function will show and hide modal when x or view button is clicked
@@ -91,22 +128,22 @@ class View extends HTMLElement {
 
     // Get event target inside a web component by getting the first item of the event path
     // When user clicks the modal container, this will match the same modal container assigned to modal variable and change the display style to none
-    // eslint-disable-next-line eqeqeq
-    if (e.path[0] == modal) {
+    // eslint-disable-next-line
+    if (e.composedPath()[0] == modal) {
       modal.style.display = "none";
     }
   }
 
   /**
      * Listen to view button when clicked and trigger showModal()
+     * showModal() pass true view card will pop up
+     * close icon is clicked view card will hide
     */
   connectedCallback() {
 
     this.shadowRoot.querySelector('#viewBtn').addEventListener('click', (e) => this.showModal(true));
-
     this.shadowRoot.querySelector('.close').addEventListener('click', (e) => this.showModal(false));
     
-
     window.addEventListener('click', (e) => this.hideModalWindow(e));
     
   }
